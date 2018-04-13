4bc8cf856fd23b6a8e667bbe161fe46948629d31
Fabricio Costa <fabricio.costa@moldeointeractive.com.ar>
2018-04-13 16:08:55 -0300

    Fixing version and package builder for debian

--- buildpkg.sh
+++ buildpkg.sh
@@ -61,14 +61,46 @@ echo "extracting..."
 
 cd deb/moldeoplayer-*
 dh_make -s -e info@moldeo.org -p moldeoplayer -c gpl3
-gedit ../../control.amd64.11.10 debian/control ../../moldeoplayer.dirs debian/moldeoplayer.dirs ../../moldeoplayer.install debian/moldeoplayer.install ../../moldeoplayer-dev.dirs debian/moldeoplayer-dev.dirs ../../moldeoplayer-dev.install debian/moldeoplayer-dev.install debian/changelog
 
-echo " 
-Now execute: 
- cd deb/moldeoplayer-*
- dpkg-buildpackage -us -uc -rfakeroot 2>&1 | tee ../../buildpkg_logs.txt
-"
+echo "Success: Extraction"
+
+sed -i -e 's/Architecture: any/Architecture: amd64/g' debian/control
+sed -i -e 's/Section: unknown/Section: graphics/g' debian/control
+sed -i -e 's/Maintainer: fabricio /Maintainer: Moldeo Interactive /g' debian/control
+sed -i -e 's/<insert the upstream URL, if relevant>/http:\\\\www.moldeo.org/g' debian/control
+#sed -i -e 's/libmoldeoBROKEN/libmoldeo/g' debian/control
+sed -i -e 's/,autotools-dev/, autotools-dev, libmoldeo-dev, libsdl2-dev, libsdl1.2-dev, freeglut3-dev/g' debian/control
+
+#development
+#sed -i -e '0,/, ${misc:Depends}/{s/, ${misc:Depends}/,${misc:Depends}, liblua5.1-0-dev, libgstreamer1.0-dev, libgstreamer-plugins-base1.0-dev, libglew-dev, libalut-dev/g}' debian/control
+#sed -i -e '0,/<insert up to 60 chars description>/{s/<insert up to 60 chars description>/Moldeo Core Library, Dev Package/}'  debian/control
+
+#binary
+sed -i -e 's/, ${misc:Depends}/,${misc:Depends}, libmoldeo, libsdl2-2.0-0, libsdl1.2debian, freeglut3/g' debian/control
+sed -i -e 's/<insert up to 60 chars description>/Moldeo Player/'  debian/control
+sed -i -e 's/<insert long description, indented with spaces>/Moldeo Player for Moldeo Software Platform/g'  debian/control
+
+sed -i -e 's/unstable/experimental/g' debian/changelog
+sed -i -e 's/fabricio/Moldeo Interactive/g' debian/changelog
+sed -i -e 's/Initial release (Closes: #nnnn)  <nnnn is the bug number of your ITP>/Initial release/g' debian/changelog
+#sed -i -e 's///g'  debian/control
+#
+#sed -i -e 's///g' debian/control
 
 
-echo "Success: extraction"
+#echo "debian/moldeoplayer/usr/bin/*" > debian/moldeoplayer.install
+
+#echo "usr/bin" > debian/moldeoplayer.dirs
+
+#echo "usr/lib" > debian/moldeoplayer.dirs
+
+#echo "usr/include/*" > debian/moldeoplayer-dev.install
+
+xed ../../control.amd64.11.10 debian/control ../../moldeoplayer.dirs debian/moldeoplayer.dirs ../../moldeoplayer.install debian/moldeoplayer.install ../../moldeoplayer-dev.dirs debian/moldeoplayer-dev.dirs ../../moldeoplayer-dev.install debian/moldeoplayer-dev.install debian/changelog
+
+dpkg-buildpackage -us -uc -rfakeroot 2>&1 | tee ../../buildpkg_logs.txt
+
+
+
+echo "Success: packaging"
 exit 0
