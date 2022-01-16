7f14ec6f5817f3456ae89159415dc5eb0d8d5662
Fabricio Costa <fabricio.costa@moldeointeractive.com>
2022-01-16 20:20:13 +0100

    Fixed transparent alpha channel always active now, for PNG rendering, video and gif with alpha

--- moldeoplayersdl2/main.cpp
+++ moldeoplayersdl2/main.cpp
@@ -460,6 +460,12 @@ int main(int argc, char** argv) {
   SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 8 );
   SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 8 );
 
+#else
+SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 8 );
+SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 8 );
+SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 8 );
+SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 8 );
+
 #endif
   //SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
   //SDL_GL_SetAttribute( SDL_GL_BUFFER_SIZE, 32 );
